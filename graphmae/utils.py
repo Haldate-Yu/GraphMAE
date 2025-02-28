3559510425b0dcfb8da4336c364b5fbb3f910916
yuwenhang <yuwenhang@123.com>
2025-02-28 17:18:30 +0800

    Update utils.py

--- graphmae/utils.py
+++ graphmae/utils.py
@@ -98,8 +98,8 @@ def build_args():
         choices=["zero", "random"],
     )
     parser.add_argument(
-        "--feature_mask_type", type=str, help="Type of missing feature mask", default="uniform",
-        choices=["uniform", "structural"],
+        "--feature_mask_type", type=str, help="Type of missing feature mask", default="original",
+        choices=["uniform", "structural", "original"],
     )
 
     # save model args
